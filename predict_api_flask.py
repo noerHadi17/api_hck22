64037a355e9fadd6ba58226d344fc8993c6cd6dd
Noerhadi <126223302+noerHadi17@users.noreply.github.com>
2025-06-16 23:40:45 +0700

    Update predict_api_flask.py

--- predict_api_flask.py
+++ predict_api_flask.py
@@ -37,5 +37,5 @@ def predict():
 
 # Run app for platform compatibility (Render)
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
